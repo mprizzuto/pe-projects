fdfe3ff446a5d48976d9407e2050e00e59d71f01
Marco R <mprizzuto88@gmail.com>
2022-02-01 18:23:44 -0500

    create site components

---
+++ components/footer.php
@@ -0,0 +1,8 @@
+		</main>
+		<footer>
+			<inner-column>
+				<h2>footer</h2>
+			</inner-column>
+		</footer>
+	</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html lang="en">
+  <?php require "components/head.php"?>
+  <body>
+    <main>  
+
+  
