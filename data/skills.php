6a63176548279ff10ef3c4604174e5e73fefc288
Marco R <mprizzuto88@gmail.com>
2022-02-01 20:36:27 -0500

    create skills associative array

---
+++ data/skills.php
@@ -0,0 +1,8 @@
+<?php 
+$skills = [
+	"HTML" => "using modern and semanric elements",
+	"CSS" => "a modular approach using the <code>import</code> feature to import many different sryle seets into one. This  improves codebase maintability ",
+	"PHP" => "PHP is used to partial out the website and template out data",
+	"JavaScript" => "JavaScript is used to add user functionality",
+];
+?>
