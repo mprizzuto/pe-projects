810a0903733c849d5117df7b4efb800a36345176
Marco R <mprizzuto88@gmail.com>
2022-02-01 20:35:47 -0500

    update header.php

--- components/header.php
+++ components/header.php
@@ -1,7 +1,23 @@
 <!doctype html>
 <html lang="en">
-  <?php require "components/head.php"?>
+  <?php 
+  require "components/head.php";
+  require "data/skills.php";
+  require "functions.php";
+  ?>
   <body>
-    <main>  
+    <main class="portfolio-main">
+      <article class="portfolio-welcome">
+        <header class="welcome">
+          <inner-column>
+            <h1 class="main-title"><span class="dev-name">Marco Rizzuto</span> Portfolio</h1>
+
+            <p>most projects were inspired by the core <a href="https://perpetual.education" target="_external">Perpetual Eduation</a> curriculum. The skills demonstarted are</p>
+
+            <ul class="skill-list">
+              <li><?=generateSkills($skills)?></li>
+            </ul>
+          </inner-column>
+        </header>
 
   
