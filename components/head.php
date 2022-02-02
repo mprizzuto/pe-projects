0db5434a4b0799b59d93530f85b288ea0b18eafc
Marco R <mprizzuto88@gmail.com>
2022-02-01 23:11:47 -0500

    format head.php

--- components/head.php
+++ components/head.php
@@ -1,10 +1,7 @@
 <head>
   <link rel="stylesheet" type="text/css" href="./styles/style.css">
 
-  
-
   <meta charset="utf-8">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
