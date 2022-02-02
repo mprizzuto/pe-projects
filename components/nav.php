dbdfdb333cf84aa95b73268c3d4cab690047dc8c
Marco R <mprizzuto88@gmail.com>
2022-02-01 23:13:38 -0500

    program nav items

--- components/nav.php
+++ components/nav.php
@@ -1,3 +1,17 @@
-<nav>
-	<!-- <a href=""></a> -->
-</nav>
+<?php 
+$navItems = [
+"home" => "index.php",
+"projects" => "?page=projects",
+"goals" => "?page=goals",
+"resume" => "?page=resume",
+];
+?>
+<header class="site-header">
+	<inner-column>
+		<nav>
+			<?php foreach($navItems as $item => $href): ?>
+				<a href="<?=$href?>"><?=$item?></a>
+			<?php endforeach; ?>
+		</nav>
+	</inner-column>
+</header>
