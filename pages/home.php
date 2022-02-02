1ccdab2f13ffb885d801bde1efb04f5407c839c1
Marco R <mprizzuto88@gmail.com>
2022-02-01 21:39:13 -0500

    create home.php

---
+++ pages/home.php
@@ -0,0 +1,11 @@
+<header class="welcome">
+  <inner-column>
+    <h1 class="main-title"><span class="dev-name">Marco Rizzuto</span> Portfolio</h1>
+
+    <p>most projects were inspired by the core <a href="https://perpetual.education" target="_external">Perpetual Eduation</a> curriculum. The skills demonstarted are</p>
+
+    <ul class="skill-list">
+      <li><?=generateSkills($skills)?></li>
+    </ul>
+  </inner-column>
+</header>
