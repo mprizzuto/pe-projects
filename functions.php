0701caf3df4c882c6ffe4f44393cb1397c7c4960
Marco R <mprizzuto88@gmail.com>
2022-02-01 21:38:56 -0500

    format generateMeta function

--- functions.php
+++ functions.php
@@ -4,4 +4,17 @@ function generateSkills(array $skills) {
 	foreach ($skills as $key => $value) 
 		echo "<li>" . "<span class='language'>$key</span> " . $value . "</li>";
 	}
+
+	// // generate the meta data for each page
+	// function generateMeta(array $data) {
+
+	// }
 ?>
+
+<?php function generateMeta(string $title, string $description, string $image) { //generate the meta data ?>
+	<title><?=$title?></title>
+
+  <meta name="description" content="<?=$description?>">
+
+  <meta property="og:image" content="<?=$image?>">
+<?php } ?>
