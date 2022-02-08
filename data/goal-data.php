e49c8ed410664d68ab93bb3e77d66737e255206f
Marco R <mprizzuto88@gmail.com>
2022-02-08 16:09:59 -0500

    add goals-data.php

---
+++ data/goal-data.php
@@ -0,0 +1,13 @@
+<?php 
+$goalList = [
+	[
+		"personal" => ["Move out of cold and overcast NYC to someplace sunny. Preferably California", "rescue a dog and cat"],
+	],
+	[
+		"professional" => ["opportunity for remote work", "be a good culture fit", "the opportunity to mentor"],
+		"company" => "I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity",
+		"expectations" => ["clear instructions", "one-on-one support", "respectuful communication", "clear deliverables and timeline"]
+	],
+
+];
+?>
