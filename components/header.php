2647cb00a718b89d23b98853ef2491513a76f884
Marco R <mprizzuto88@gmail.com>
2022-02-08 15:53:01 -0500

    put page logic in main index.php

--- components/header.php
+++ components/header.php
@@ -33,24 +33,5 @@
     <main class="<?=$getPage?>">
       <article>
         
-      <?php
-        
-        //get the pages
-        switch($getPage) {
-          case "projects":
-            include "pages/projects.php";
-            break;
-
-          case "goals":
-            include "pages/goals.php";
-            break;
-
-          case "resume":
-            include "pages/resume.php";
-            break;
-
-          default:
-            include "pages/home.php";
-        }
-        ?>
+      
   
--- index.php
+++ index.php
@@ -2,7 +2,26 @@
 require "components/header.php";
 ?>
 
+<?php
+        
+//get the pages
+switch($getPage) {
+  case "projects":
+    include "pages/projects.php";
+    break;
 
+  case "goals":
+    include "pages/goals.php";
+    break;
+
+  case "resume":
+    include "pages/resume.php";
+    break;
+
+  default:
+    include "pages/home.php";
+}
+?>
 <?php 
 require "components/footer.php";
 ?>
