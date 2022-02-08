760b7f3a5ce4154db9f6d02f156b88bc6b11ccac
Marco R <mprizzuto88@gmail.com>
2022-02-08 16:23:14 -0500

    write resume-data.php

---
+++ data/resume-data.php
@@ -0,0 +1,13 @@
+<?php 
+$resumeData = [
+	[
+		"education" => "queensboro community college. BA",
+		"experience" => [
+			"Perpetual Education Beta tester. My responsibilities included", "colloboration and feedback on lesson content", "accessibility testing: offering feedback from the perspective of someone with learning differences"],
+	],
+	[
+	  "non-technical-skills" => ["writing", "proofreading", "public speaking", "photography"],
+		"technical-skills" => ["HTML", "CSS", "PHP", "JavaScript", "Git"],
+	]
+];
+?>
