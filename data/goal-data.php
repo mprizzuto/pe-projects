fec639717549afd21b7f8711a83d472752bc9a80
Marco R <mprizzuto88@gmail.com>
2022-02-09 12:06:36 -0500

    chane data structures for goals and resume

--- data/goal-data.php
+++ data/goal-data.php
@@ -1,12 +1,12 @@
 <?php 
 $goalList = [
 	[
-		"personal" => ["Move out of cold and overcast NYC to someplace sunny. Preferably California", "rescue a dog and cat"],
+		"personal" => "Move out of cold and overcast NYC to someplace sunny. Preferably California <br> rescue a dog and cat",
 	],
 	[
-		"professional" => ["opportunity for remote work", "be a good culture fit", "the opportunity to mentor"],
+		"professional" => "opportunity for remote work <br>be a good culture fit <br>the opportunity to mentor",
 		"company" => "I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity",
-		"expectations" => ["clear instructions", "one-on-one support", "respectuful communication", "clear deliverables and timeline"]
+		"expectations" => "clear instructions <br> one-on-one support <br>respectuful communication<br> clear deliverables and timeline"
 	],
 
 ];
