8f349049c33924995a26aab22eade4c90b4421d6
Marco R <mprizzuto88@gmail.com>
2022-02-09 12:05:58 -0500

    update resume and goals.php

--- pages/resume.php
+++ pages/resume.php
@@ -1,1 +1,10 @@
-resume.php
+<?php 
+require "./data/resume-data.php";
+require_once "./functions.php";
+?>
+
+<section class="resume-data">
+	<inner-column>
+		<?php generateResume($resumeData) ?>
+	</inner-column>
+</section>
