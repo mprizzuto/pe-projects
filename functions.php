f06e4e1bb0ae0d10b30574e0ebc122506b08e4a5
Marco R <mprizzuto88@gmail.com>
2022-02-11 18:33:59 -0500

    delete generateGoals function

--- functions.php
+++ functions.php
@@ -33,14 +33,6 @@ function generateSkills(array $skills) {
 	<?php endforeach ?>
 <?php } ?>
 
-<?php function generateGoals(iterable $goalData) { ?>
- <?php foreach($goalData as $data): ?>
- 	<?php foreach($data as $key => $value): ?>
- 		<h2><?=$key?> </h2>
- 		<p><?=$value?></p>
- 	<?php endforeach; ?>
- <?php endforeach; ?>
-<?php } ?>
 
 
 
