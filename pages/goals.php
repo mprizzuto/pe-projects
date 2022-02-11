a97c21d263b53403e7c1865474f2153fcb828475
Marco R <mprizzuto88@gmail.com>
2022-02-11 18:33:44 -0500

    template out goals page

--- pages/goals.php
+++ pages/goals.php
@@ -4,6 +4,50 @@ require "./data/goal-data.php";
 ?>
 <section class="goal-data">
 	<inner-column>
-		<?php generateGoals($goalData) ?>
+  <ul class="goal-list">
+	  <li><strong>personal</strong>
+	  	<ul class="goal-expanded">
+	  		<li><?php foreach($goalData as $data) {?>
+				<?php 
+	      for($i = 0; $i < count($data); $i++ ) {
+	        echo $data["personal"][$i] ?? null;
+	      }
+				?>
+	    <?php }?></li>
+	  	</ul>
+	  </li>
+		  
+	  <li><strong>professional</strong>
+	  	<ul class="goal-expanded">
+	  		<li>
+	  			<?php foreach($goalData as $data) {?>
+			<?php 
+	    for($i = 0; $i < count($data); $i++ ) {
+	      echo $data["professional"][$i] ?? null;
+	    }
+			?>
+	    <?php }?>
+	  		</li>
+	  	</ul>
+	  </li>
+
+	  <li><strong>expectations</strong>
+	  	<ul class="goal-expanded">
+	  		<li>
+	  			<?php foreach($goalData as $data) {?>
+			<?php 
+	    for($i = 0; $i < count($data); $i++ ) {
+	      echo $data["expectations"][$i] ?? null ;
+	    }
+			?>
+	    <?php }?>
+	  		</li>
+	  	</ul>
+	  </li>
+
+		  <li><strong>at the end of the day</strong>
+		  	<?=$data["company"] ?? null?>
+		  </li>
+		</ul>
 	</inner-column>
 </section>
