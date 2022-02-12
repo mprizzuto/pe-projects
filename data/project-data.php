4c282847db37b6891a5de3100eb014fdd0b01de1
Marco R <mprizzuto88@gmail.com>
2022-02-11 20:39:13 -0500

    create projects data structures

---
+++ data/project-data.php
@@ -0,0 +1,53 @@
+<?php 
+
+$projects = [
+  [
+  	"name" => "web portal",
+		"decription" => "this is a web portal",
+		"link" => "?page=project&project-name=web-portal",
+  ],
+  [
+	  "name" => "svg",
+	  "decription" => "my SVG progress",
+	  "link" => "?page=project&project-name=svg",
+  ],
+  [
+	  "name" => "mock-client",
+	  "decription" => "this is a mock client website",
+	  "link" => "?page=project&project-name=mock-client",
+  ],
+  [
+	  "name" => "responsive-layout",
+	  "decription" => "the responsive-layout challenge",
+	  "link" => "?page=project&project-name=responsive-layout",
+  ],
+  [
+	  "name" => "responsive-layout-garden",
+	  "decription" => "the responsive-layout garden",
+	  "link" => "?page=project&project-name=responsive-layout-garden",
+  ],
+  [
+	  "name" => "monster adoption agency",
+	  "decription" => "check out the monster adoption agency",
+	  "link" => "?page=project&project-name=monster-adoption-agency",
+  ],
+  [
+	  "name" => "theme challenge",
+	  "decription" => "the theme challenge. CSS!",
+	  "link" => "?page=project&project-name=theme-challenge",
+  ],
+  [
+	  "name" => "Exercises for Programmers",
+	  "decription" => "come see my programming skills",
+	  "link" => "?page=project&project-name=efp",
+  ],
+];
+
+/*
+[
+  "name" => "",
+  "decription" => "",
+  "link" => "?page=project&project-name=",
+]
+*/ 
+
