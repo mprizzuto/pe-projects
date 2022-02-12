c6ea88c51d859303634fb5e1e0fcde142f6c555c
Marco R <mprizzuto88@gmail.com>
2022-02-11 20:39:28 -0500

    add list items to goals data

--- data/goal-data.php
+++ data/goal-data.php
@@ -13,12 +13,12 @@
 
 $goalData = [
 	[
-		"personal" => ["Move out of cold and overcast NYC to someplace sunny. Preferably California", "rescue a dog and cat"],
+		"personal" => ["<li>Move out of cold and overcast NYC to someplace sunny. Preferably California</li>", "<li>rescue a dog and cat</li>"],
 	], 
 	[
-		"professional" => ["opportunity for remote work", "be a good culture fit <br>the opportunity to mentor"],
-		"company" => "I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity",
-		"expectations" => ["clear instructions <br>", "one-on-one support <br>", "respectuful communication <br>", "clear deliverables and timeline <br>"], 
+		"professional" => ["<li>opportunity for remote work </li>", "<li>be a good culture fit</li>", "<li>the opportunity to mentor</li>"],
+		"company" => "<p>I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity</p>",
+		"expectations" => ["<li>clear instructions</li>", "<li>one-on-one support </li>", "<li>respectuful communication </li>", "<li>clear deliverables and timeline</li>"], 
 	],
 
 ];
