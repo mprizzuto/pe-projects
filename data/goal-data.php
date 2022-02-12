047fb543d1d966fd27b43868ba3abccb5acc9555
Marco R <mprizzuto88@gmail.com>
2022-02-12 09:57:51 -0500

    write css for list items

--- data/goal-data.php
+++ data/goal-data.php
@@ -10,16 +10,26 @@
 // 	],
 
 // ];
+//TODO: remove list items from array
+// $goalData = [
+// 	[
+// 		"personal" => ["<li>Move out of cold and overcast NYC to someplace sunny. Preferably California</li>", "<li>rescue a dog and cat</li>", "<li>smile more</li>", "<li>laugh more</li>"],
+// 	], 
+// 	[
+// 		"professional" => ["<li>opportunity for remote work </li>", "<li>be a good culture fit</li>", "<li>the opportunity to mentor</li>", "<li>jumping jacks</li>"],
+// 		"company" => ["<p>I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity</p>"],
+// 		"expectations" => ["<li>clear instructions</li>", "<li>one-on-one support </li>", "<li>respectuful communication </li>", "<li>clear deliverables and timeline</li>"], 
+// 	],
+// ];
 
 $goalData = [
 	[
-		"personal" => ["<li>Move out of cold and overcast NYC to someplace sunny. Preferably California</li>", "<li>rescue a dog and cat</li>"],
+		"personal" => ["Move out of cold and overcast NYC to someplace sunny. Preferably California", "rescue a dog and cat", "smile more", "laugh more"],
 	], 
 	[
-		"professional" => ["<li>opportunity for remote work </li>", "<li>be a good culture fit</li>", "<li>the opportunity to mentor</li>"],
-		"company" => "<p>I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity</p>",
-		"expectations" => ["<li>clear instructions</li>", "<li>one-on-one support </li>", "<li>respectuful communication </li>", "<li>clear deliverables and timeline</li>"], 
+		"professional" => ["opportunity for remote work", "be a good culture fit", "the opportunity to mentor", "jumping jacks"],
+		"company" => ["I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity"],
+		"expectations" => ["clear instructions", "one-on-one support ", "respectuful communication ", "clear deliverables and timeline"], 
 	],
-
 ];
 ?>
