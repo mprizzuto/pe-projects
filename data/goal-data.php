3a715bd631b17de7b594fb12b0b4c4e3f286064e
Marco R <mprizzuto88@gmail.com>
2022-02-12 09:59:20 -0500

    fix data structures for goals page

--- data/goal-data.php
+++ data/goal-data.php
@@ -1,27 +1,4 @@
 <?php
-// $goalData = [
-// 	[
-// 		"personal" => "Move out of cold and overcast NYC to someplace sunny. Preferably California <br> rescue a dog and cat",
-// 	],
-// 	[
-// 		"professional" => "opportunity for remote work <br>be a good culture fit <br>the opportunity to mentor",
-// 		"company" => "I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity",
-// 		"expectations" => "clear instructions <br> one-on-one support <br>respectuful communication<br> clear deliverables and timeline"
-// 	],
-
-// ];
-//TODO: remove list items from array
-// $goalData = [
-// 	[
-// 		"personal" => ["<li>Move out of cold and overcast NYC to someplace sunny. Preferably California</li>", "<li>rescue a dog and cat</li>", "<li>smile more</li>", "<li>laugh more</li>"],
-// 	], 
-// 	[
-// 		"professional" => ["<li>opportunity for remote work </li>", "<li>be a good culture fit</li>", "<li>the opportunity to mentor</li>", "<li>jumping jacks</li>"],
-// 		"company" => ["<p>I want to work for a company in the medical or education field. I feel like these two fields have the most impact on humanity</p>"],
-// 		"expectations" => ["<li>clear instructions</li>", "<li>one-on-one support </li>", "<li>respectuful communication </li>", "<li>clear deliverables and timeline</li>"], 
-// 	],
-// ];
-
 $goalData = [
 	[
 		"personal" => ["Move out of cold and overcast NYC to someplace sunny. Preferably California", "rescue a dog and cat", "smile more", "laugh more"],
